9c315f3047e76c8422ae2918c1820437689c55d6
Aabha <aabha.khandwala@gmail.com>
2020-08-13 22:00:32 -0400

    initial commit

---
+++ OrganixProduce-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+import { UsersComponent } from './admin/users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdduserComponent } from './admin/users/adduser/adduser.component';
+import { ViewuserComponent } from './admin/users/viewuser/viewuser.component';
+
+@NgModule({
+  declarations: [AppComponent, NavComponent, UsersComponent, AdduserComponent, ViewuserComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
